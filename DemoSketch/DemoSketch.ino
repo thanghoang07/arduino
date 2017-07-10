ad31e75a4a30e367cf02ec84c67e68e74b5fc16d
Hoang Duc Thang <thanghoang07@gmail.com>
2017-07-10 16:35:34 +0700

    Add project files.

---
+++ DemoSketch/DemoSketch.ino
@@ -0,0 +1,22 @@
+﻿/*
+ Name:		DemoSketch.ino
+ Created:	7/10/2017 4:22:55 PM
+ Author:	thang
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	//Đầu tiên là khai báo pin đầu ra ở vị trí pin 13.
+	pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	//thực hiện việc bật đèn LED lên
+	digitalWrite(13, HIGH);
+	delay(1000);
+	//Chờ 1 giây và sau đó thực hiện việc tắt đèn LED đi
+	digitalWrite(13, LOW);
+	delay(1000);
+	//Chờ 1 giây và thực hiện lặp lại như trên.
+}
